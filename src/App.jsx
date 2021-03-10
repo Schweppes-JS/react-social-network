e8760b24e5c6720b6007b0d803a691f4b645ce7a
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-03-10 23:47:21 +0200

    made build

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ const AppContainer = compose(
 )(App);
 
 const MainApp = props => {
-  return <BrowserRouter>
+  return <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <AppContainer />
     </Provider>
